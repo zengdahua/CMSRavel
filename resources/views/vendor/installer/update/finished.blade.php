75cc2419296a91f995278c0ea9a8406e2e4ecc64
Mr.L <admin@duxphp.com>
2021-06-24 19:04:20 +0800

    安装模板内置

--- resources/views/vendor/installer/update/finished.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('vendor.installer.layouts.master-update')
-
-@section('title', trans('installer_messages.updater.final.title'))
-@section('container')
-    <p class=" text-center">{{ session('message')['message'] }}</p>
-    <div class="py-6 text-right">
-        <a href="{{ url('/') }}" class="btn-blue">{{ trans('installer_messages.updater.final.exit') }}</a>
-    </div>
-@stop
--- resources/views/vendor/installer/update/overview.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('vendor.installer.layouts.master-update')
-
-@section('title', trans('installer_messages.updater.welcome.title'))
-@section('container')
-    <p class=" text-center">{{ trans_choice('installer_messages.updater.overview.message', $numberOfUpdatesPending, ['number' => $numberOfUpdatesPending]) }}</p>
-    <div class="py-6 text-right">
-        <a href="{{ route('LaravelUpdater::database') }}" class="btn-blue">{{ trans('installer_messages.updater.overview.install_updates') }}</a>
-    </div>
-@stop
